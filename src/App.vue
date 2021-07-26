4fd3b6d314c80d1bb0abf48b78106429cec1458c
Emanoel Cardoso <emanoel97@hotmail.com>
2021-07-26 18:24:06 -0300

    Adicionado esqueleto da parte principal, e commit antes de concertar bacground no app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,23 @@
       <div class="">
         <Navbar/>
       </div>
+
+      <div>
+        <Maingames/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Maingames from './components/Maingames.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Maingames
   }
 }
 </script>
---
+++ src/components/Maingames.vue
@@ -0,0 +1,297 @@
+<template lang="">
+    <div>
+        <div class="mt-10 w-4/5 mx-auto my-0">
+            <div class="grid grid-cols-3">
+                <div class=" col-span-2 grid grid-cols-2">
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game1.png" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game2.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game3.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game4.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game5.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game6.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game7.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game8.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game9.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game10.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game11.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game12.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game13.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game14.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game15.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game16.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game17.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game18.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game19.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center items-center">
+                        <div class="">
+                            <img src="img/game20.jpg" class="w-4/5 mx-auto mx-0">    
+                        </div>
+                        <div>
+                            <span class="font-kanit text-white text-2xl">Title title title</span>
+                        </div>
+                        <div class="flex text-gray-200 font-kanit text-sm">
+                            <span>About</span>
+                            <span>x</span>
+                            <span>Time</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-4/5 bg-white col-span-1">b</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
